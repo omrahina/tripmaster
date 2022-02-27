ad5d7719028dc512ccca220d186fd35728d908ef
omrahina <crave627@gmail.com>
2022-02-27 19:31:37 +0100

    Location ms : add Location entry controller + exceptions

--- Location/src/main/java/com/tripmaster/location/controller/TrackerController.java
+++ Location/src/main/java/com/tripmaster/location/controller/TrackerController.java
@@ -10,10 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -40,17 +37,25 @@ public class TrackerController {
     public ResponseEntity<VisitedLocation> getLocation(@RequestParam String username) {
         VisitedLocation visitedLocation = null;
         try {
-            UserLocation location = getUserLocation(username);
-            if (location == null) {
-                location = userLocationService.addLocation(new UserLocation(UUID.randomUUID(), username));
-            }
+            UserLocation location = userLocationService.findLocationEntryByUsername(username);
             visitedLocation = trackerService.getUserLocation(location);
         } catch (UserLocationException e) {
             throw new ResponseStatusException(
-                    HttpStatus.INTERNAL_SERVER_ERROR, "Please retry", e);
+                    HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
         LOGGER.info("User location detected");
         return new ResponseEntity<>(visitedLocation, HttpStatus.OK);
+    }
+
+    @PostMapping("/location")
+    public  ResponseEntity<UserLocation> addLocationEntry(@RequestParam String username) {
+        try {
+            UserLocation locationEntry = userLocationService.addLocation(new UserLocation(UUID.randomUUID(), username));
+            return new ResponseEntity<>(locationEntry, HttpStatus.CREATED);
+        } catch (UserLocationException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT, e.getMessage(), e);
+        }
     }
 
     @GetMapping("/allKnownLocations")
@@ -66,15 +71,13 @@ public class TrackerController {
 
     @GetMapping("/locations")
     public ResponseEntity<List<VisitedLocation>> getUserLocations(@RequestParam String username) {
-        UserLocation location = getUserLocation(username);
-        if (location != null) {
+        try {
+            UserLocation location = userLocationService.findLocationEntryByUsername(username);
             return new ResponseEntity<>(location.getVisitedLocations(), HttpStatus.OK);
+        } catch (UserLocationException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, e.getMessage(), e);
         }
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No location entry found for " + username);
-    }
-
-    private UserLocation getUserLocation(String username) {
-        return userLocationService.findLocationEntryByUsername(username);
     }
 
 }
--- Location/src/main/java/com/tripmaster/location/service/UserLocationService.java
+++ Location/src/main/java/com/tripmaster/location/service/UserLocationService.java
@@ -1,6 +1,7 @@
 package com.tripmaster.location.service;
 
 import com.tripmaster.location.data.MockLocationDataUtils;
+import com.tripmaster.location.exceptions.UserLocationException;
 import com.tripmaster.location.model.UserLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +15,7 @@ public class UserLocationService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserLocationService.class);
 
-    public UserLocation addLocation(UserLocation userLocation) {
+    public UserLocation addLocation(UserLocation userLocation) throws UserLocationException{
         if(!MockLocationDataUtils.getInternalUserLocationMap().containsKey(userLocation.getUsername())) {
             MockLocationDataUtils.addLocationEntry(userLocation);
             LOGGER.info("Location added for user {}", userLocation.getUsername());
@@ -22,17 +23,17 @@ public class UserLocationService {
         }
         LOGGER.warn("Location entry for user {} already exists", userLocation.getUsername());
 
-        return null;
+        throw new UserLocationException("Location entry already exists");
     }
 
-    public UserLocation findLocationEntryByUsername(String username) {
+    public UserLocation findLocationEntryByUsername(String username) throws UserLocationException {
         UserLocation userLocation = MockLocationDataUtils.getInternalUserLocationMap().get(username);
         if (userLocation != null) {
             LOGGER.info("Location entry found");
             return userLocation;
         }
         LOGGER.warn("Location entry not found");
-        return null;
+        throw new UserLocationException("Location entry not found");
     }
 
     public List<UserLocation> getAllUserLocations() {
